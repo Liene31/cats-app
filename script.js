c7d455987a9edfd10b89db901242f4fe158ef690
Liene <l.bare@interface3.be>
2025-09-26 13:18:26 +0200

    add dark btn in the hamburger menu via js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const menuUl = document.getElementById("menu");
 const menuIcon = document.getElementById("menu-icon");
 const closeIcon = document.getElementById("close-icon");
+let hamburgerMoonLogo = document.getElementById("hamburger-moon-logo");
 let isShowMenu = false;
 
 // Hamburger Button - Toggles the menu
@@ -10,10 +11,14 @@ document.getElementById("hamburger-btn").addEventListener("click", () => {
     closeIcon.style.display = "block";
     menuIcon.style.display = "none";
     isShowMenu = true;
+    hamburgerMoonLogo.innerHTML = `<a href="#">
+        <img src="./images/moon-logo.jpg" alt="moon logo" width="40px" />
+      </a>`;
   } else {
     menuUl.classList.remove("show-menu");
     closeIcon.style.display = "none";
     menuIcon.style.display = "block";
+    hamburgerMoonLogo.innerHTML = "";
     isShowMenu = false;
   }
 });
